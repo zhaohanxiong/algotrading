36a45d145c3c0cf04cc1f645d5101f4a195a2e92
zhaohanxiong <zhaohanx@hotmail.com>
2025-01-14 18:49:47 +1100

    made a class for downloading

--- algotrader/data/downloader.py
+++ algotrader/data/downloader.py
@@ -5,23 +5,39 @@ from argparse import ArgumentParser
 
 DOWNLOADER_FILE = "algotrader/data/binanceDownloader/download-kline.py"
 
+class DataDownloader:
 
-def download(
-    downloader_file: str = DOWNLOADER_FILE,
-    symbols: str = "BTCUSDT ETHUSDT",
-    start_date: str = "2025-01-01",
-    end_date: str = "2025-01-05",
-):
+    def __init__(
+        self,
+        symbols: str = "BTCUSDT ETHUSDT",
+        start_date: str = "2025-01-01",
+        end_date: str = "2025-01-05"
+    ):
+        self.downloader_script = DOWNLOADER_FILE
+        self.symbols = symbols
+        self.start_date = start_date
+        self.end_date = end_date
+    
+    def data_download(self):
 
-    args = f"-t spot -s {symbols} -startDate {start_date} -endDate {end_date} -i 1s -skip-monthly 1"
-    os.system(f"python {downloader_file} {args}")
+        args = f"-t spot -s {self.symbols} -startDate {self.start_date} -endDate {self.end_date} -i 1s -skip-monthly 1"
+        os.system(f"python {self.downloader_script} {args}")
 
-    return
+    def data_preprocess(self):
+
+        # go through each symbol
+        # go through each date
+        # unzip
+        # read into pandas, apply column headings
+
+        # set up new file location
+        # save to new location with symbole/date
+
+        return
 
 if __name__ == "__main__":
 
     parser = ArgumentParser(description=("Download historical data"))
-
     parser.add_argument(
         '-s', dest='symbols', nargs='+',
         help='Single symbol or multiple symbols separated by space')
@@ -34,11 +50,11 @@ if __name__ == "__main__":
     parser.add_argument(
         '-folder', dest='folder',
         help='Directory to store the downloaded data')
-    
     args = parser.parse_args(sys.argv[1:])
 
-    download(
+    downloader = DataDownloader(
         symbols = " ".join(args.symbols),
         start_date = args.startDate,
         end_date = args.endDate
     )
+    downloader.data_download()
