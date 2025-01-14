80cd79d9ab1c763d017800fa9543b7fd126927b1
zhaohanxiong <zhaohanx@hotmail.com>
2025-01-14 21:56:42 +1100

    not working yet: implemented full data downloading

--- algotrader/data/downloader.py
+++ algotrader/data/downloader.py
@@ -1,39 +1,85 @@
 import os
 import sys
+import shutil
+import pandas as pd
+import zipfile
 from argparse import ArgumentParser
 
 
-DOWNLOADER_FILE = "algotrader/data/binanceDownloader/download-kline.py"
+
+DOWNLOADER_DIR = "algotrader/data/binanceDownloader"
+DOWNLOADER_FILE = os.path.join(DOWNLOADER_DIR, "/download-kline.py")
+
+CLEANED_DIR = "CRYPTO_DAILY_DATA"
+
+COLUMN_HEADINGS = [
+    "Open_time", 
+    "Open", "High", "Low", "Close", "Volume",
+    "Close_time",
+    "Quote_asset_volume", "Number_of_trades",
+    "Taker_buy_base_asset_volume", "Taker_buy_quote_asset_volume",
+    "Ignore"
+]
+
+
 
 class DataDownloader:
 
     def __init__(
         self,
-        symbols: str = "BTCUSDT ETHUSDT",
+        symbols: list[str] = ["BTCUSDT", "ETHUSDT"],
         start_date: str = "2025-01-01",
-        end_date: str = "2025-01-05"
+        end_date: str = "2025-01-05",
+        market: str = "spot",
+        sample_rate: str = "1s",
+        data_type: str = "klines",
     ):
         self.downloader_script = DOWNLOADER_FILE
         self.symbols = symbols
-        self.start_date = start_date
-        self.end_date = end_date
+        self.start_date = pd.to_datetime(start_date)
+        self.end_date = pd.to_datetime(end_date)
+        self.market = market
+        self.sample_rate = sample_rate
+        self.data_type = data_type
     
     def data_download(self):
+        os.system(
+            (
+                f"python {self.downloader_script} "
+                f"-t {self.market} -s {" ".join(self.symbols)} "
+                f"-startDate {self.start_date.strftime('%Y-%m-%d')} "
+                f"-endDate {self.end_date.strftime('%Y-%m-%d')} "
+                f"-i {self.sample_rate} -skip-monthly 1"
+            )
+        )
 
-        args = f"-t spot -s {self.symbols} -startDate {self.start_date} -endDate {self.end_date} -i 1s -skip-monthly 1"
-        os.system(f"python {self.downloader_script} {args}")
+    def data_clean(self):
+        
+        base_path = f"{DOWNLOADER_DIR}\data\{self.market}\daily\klines"
 
-    def data_preprocess(self):
+        for symbol in self.symbols:
+            
+            os.mkdir(f"{CLEANED_DIR}\{symbol}")
 
-        # go through each symbol
-        # go through each date
-        # unzip
-        # read into pandas, apply column headings
+            symbol_path = (
+                f"{base_path}\{symbol}\{self.sample_rate}\\"
+                f"{self.start_date.strftime('%Y-%m-%d')}_{self.end_date.strftime('%Y-%m-%d')}"
+            )
 
-        # set up new file location
-        # save to new location with symbole/date
+            d_from, d_to = self.start_date, self.end_date - pd.timedelta(days=1)
+            for date in pd.date_range(d_from, d_to, freq='d'):
+                
+                file_name = f"{symbol}-{self.sample_rate}-{date.strftime('%Y-%m-%d')}"
+                zip_file = zipfile.ZipFile(f"{symbol_path}\{file_name}.zip", 'r')
 
-        return
+                df = pd.read_csv(zip_file.open(f'{file_name}.csv'), header=None)
+                df.columns = COLUMN_HEADINGS
+
+                df.to_parquet(f"CLEANED_DIR\{symbol}\{date.strftime('%Y-%m-%d')}.parquet")
+
+            shutil.rmtree(f"{base_path}\{symbol}")
+
+
 
 if __name__ == "__main__":
 
@@ -50,11 +96,12 @@ if __name__ == "__main__":
     parser.add_argument(
         '-folder', dest='folder',
         help='Directory to store the downloaded data')
+    
     args = parser.parse_args(sys.argv[1:])
-
     downloader = DataDownloader(
-        symbols = " ".join(args.symbols),
+        symbols = args.symbols,
         start_date = args.startDate,
         end_date = args.endDate
     )
     downloader.data_download()
+    downloader.data_clean()
